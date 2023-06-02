c66f4b270e6ef2bbcd403ba961678003f25c5c5c
carlostavares <carlostavares2350@gmail.com>
2023-06-02 19:27:27 -0300

    Adicionando novo projeto App_musica com poo

---
+++ App_musica_C#/App_musica_C#/Program.cs
@@ -0,0 +1,13 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Carlos";
+musica1.artista = "Vocal";
+musica1.duracao = 273;
+musica1.disponível = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Priscila";
+musica2.artista = "Amada";
+musica2.duracao = 173;
+musica2.disponível = false;
+
+musica2.ExibirFichaTecnica();
---
+++ App_musica_C#/App_musica_C#/musica.cs
@@ -0,0 +1,25 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponível;
+
+    public void ExibirFichaTecnica()
+
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponível)
+        {
+            Console.WriteLine("Disponível para plano!");
+        }else
+        {
+            Console.WriteLine("Não disponível para plano!");
+        }
+    }
+
+
+
+} 
