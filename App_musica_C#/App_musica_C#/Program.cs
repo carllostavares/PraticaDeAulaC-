41736c9082c5fb0c63f133922326929c7b276946
carlostavares <carlostavares2350@gmail.com>
2023-06-05 16:13:00 -0300

    Criandao um Construtor naa Class musica. Onde será adicionado o nome do Artista so criar uma nova musica

--- App_musica_C#/App_musica_C#/Program.cs
+++ App_musica_C#/App_musica_C#/Program.cs
@@ -1,19 +1,21 @@
-﻿Album albumDoQueem = new Album();
+﻿Banda queem = new Banda();
+queem.Nome = "Queem";
+
+Album albumDoQueem = new Album();
 albumDoQueem.Nome = "A night at the opera";
 
-Musica musica1 = new Musica();
+Musica musica1 = new Musica(queem);
 musica1.Nome = "Love of my life";
 musica1.Duracao = 213;
 
-Musica musica2 = new Musica();
+Musica musica2 = new Musica(queem);
 musica2.Nome = "Bohemian Rhapsody";
 musica2.Duracao = 354;
 
 albumDoQueem.AdicionarMusica(musica1);
 albumDoQueem.AdicionarMusica(musica2);
 
-Banda queem = new Banda();
-queem.Nome = "Queem";
+
 
 queem.AdicionaAlbum(albumDoQueem);
 queem.ExibirDicografia();
