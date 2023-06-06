6ef91230446da369e92e9014b87d3f3a6aa793bf
carlostavares <carlostavares2350@gmail.com>
2023-06-06 11:43:56 -0300

    Adicionando class Podecast

--- App_musica_C#/App_musica_C#/Podecast.cs
+++ App_musica_C#/App_musica_C#/Podecast.cs
@@ -1,1 +1,21 @@
-﻿
+﻿class Podecast
+{
+    private List<Episodio> episodios = new List<Episodio>();
+    public Podecast(string nome, string host)
+    {
+        Nome = nome;
+        Host = host;
+    }
+
+    public string Nome { get; set; }
+    public string Host { get; set; }
+
+    public int TotalEpisodio { get; set; }
+
+    public void AdicionaEpisodio(Episodio episodio)
+    {
+        episodios.Add(episodio);
+        
+    }
+
+}
