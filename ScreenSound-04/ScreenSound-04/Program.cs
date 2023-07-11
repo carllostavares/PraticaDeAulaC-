ddf52c4df089268523ee190b5016d414c0ea5cb0
carlostavares <carlostavares2350@gmail.com>
2023-07-11 18:35:31 -0300

    Criando projeto ScreenSound-04.

---
+++ ScreenSound-04/ScreenSound-04/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Net.Http.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    Console.WriteLine(resposta);
+}
---
+++ ScreenSound03-projeto-inicial/ScreenSound/Menus/AvaliarAlbum.cs
@@ -0,0 +1,12 @@
+﻿
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class AvaliarAlbum : Menus
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+    }
+}
