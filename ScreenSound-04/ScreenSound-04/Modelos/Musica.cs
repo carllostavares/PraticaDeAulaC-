6a3fd91c523c6571e2451b8568cd03b17b21959a
carlostavares <carlostavares2350@gmail.com>
2023-07-12 22:05:14 -0300

    Adicionando a Desserialização dos dados no Program.cs do projeto ScreenSound-04.

--- ScreenSound-04/ScreenSound-04/Modelos/Musica.cs
+++ ScreenSound-04/ScreenSound-04/Modelos/Musica.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Text.Json.Serialization;
 
-namespace ScreenSound_04.Modelos
+namespace ScreenSound_04.Modelos;
+
+internal class Musica
 {
-    internal class Musica
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int? Duracao { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalhesDaMusica()
     {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Música: {Artista}");
+        Console.WriteLine($"Duração em segundos: {Duracao/1000}");
+        Console.WriteLine($"Genero: {Genero}");
+
     }
 }
--- ScreenSound-04/ScreenSound-04/Program.cs
+++ ScreenSound-04/ScreenSound-04/Program.cs
@@ -1,11 +1,16 @@
-﻿using System.Net.Http.Json;
+﻿using ScreenSound_04.Modelos;
+using System.Net.Http.Json;
+using System.Text.Json;
 
 using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+
+        var musica = JsonSerializer.Deserialize<List<Musica>>(resposta);
+
+        musica[0].ExibirDetalhesDaMusica();
     }
     catch (Exception ex) 
     {
