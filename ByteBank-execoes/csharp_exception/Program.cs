ba528d63c8b4a5c5b4c78fbb7f8aaabc4dda8a8c
carlostavares <carlostavares2350@gmail.com>
2023-07-19 17:14:44 -0300

    Adicionando novo projeto ByteBank-execoes para exercitar a aplicabilidaade de exerções no C#.

---
+++ ByteBank-execoes/csharp_exception/Program.cs
@@ -0,0 +1,18 @@
+﻿using csharp_exception.Titular;
+using csharp_exception.Contas;
+
+
+try
+{
+    ContaCorrente conta1 = new ContaCorrente(283, "1234-X");
+    Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+}
+catch(ArgumentException ex)
+{
+    Console.WriteLine("Parâmetro com erro" + ex.ParamName);
+    Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
+    Console.WriteLine(ex.Message);
+    
+}
+
