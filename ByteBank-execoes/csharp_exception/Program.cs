e7c877c0e993f5eaa149ff7bd91094db3d5edfbf
carlostavares <carlostavares2350@gmail.com>
2023-07-27 18:55:34 -0300

    Adicionando classe OperacaoFinanceiraException e SaldoInsuficienteException projeto ByteBank-execoes corrigido.

--- ByteBank-execoes/csharp_exception/Program.cs
+++ ByteBank-execoes/csharp_exception/Program.cs
@@ -1,6 +1,6 @@
 ﻿using csharp_exception.Titular;
 using csharp_exception.Contas;
-
+using csharp_exception;
 
 try
 {
@@ -10,7 +10,13 @@ catch(ArgumentException ex)
 {
     Console.WriteLine("Parâmetro com erro" + ex.ParamName);
     Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
+    Console.WriteLine(ex.StackTrace);
     Console.WriteLine(ex.Message);
     
+}
+catch(SaldoInsuficienteException ex)
+{
+    Console.WriteLine("Operação negada ! Saldo Insuficiente !");
+    Console.WriteLine(ex.Message);
 }
 
---
+++ ByteBank-execoes/csharp_exception/SaldoInsuficienteException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exception;
+
+internal class SaldoInsuficienteException : OperacaoFinanceiraException
+{
+    public SaldoInsuficienteException(string mensagem) : base(mensagem)
+    {
+
+    }
+}
