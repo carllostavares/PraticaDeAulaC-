ccdf5fafb37c36f45c8615a95aa73852f6e15d6e
carlostavares <carlostavares2350@gmail.com>
2023-07-28 14:18:29 -0300

    Adicionando classe LeitorDeArquivo no projeto ByteBank-execoes corrigido.

--- ByteBank-execoes/csharp_exception/Program.cs
+++ ByteBank-execoes/csharp_exception/Program.cs
@@ -2,21 +2,24 @@
 using csharp_exception.Contas;
 using csharp_exception;
 
-try
-{
-    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
-}
-catch(ArgumentException ex)
-{
-    Console.WriteLine("Parâmetro com erro" + ex.ParamName);
-    Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
-    Console.WriteLine(ex.StackTrace);
-    Console.WriteLine(ex.Message);
-    
-}
-catch(SaldoInsuficienteException ex)
-{
-    Console.WriteLine("Operação negada ! Saldo Insuficiente !");
-    Console.WriteLine(ex.Message);
-}
+LeitorDeArquivo leitor = new LeitorDeArquivo("contasa.txt");
+leitor.LerProximaLinha();
+
+//try
+//{
+//    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
+//}
+//catch (ArgumentException ex)
+//{
+//    Console.WriteLine("Parâmetro com erro" + ex.ParamName);
+//    Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
+//    Console.WriteLine(ex.StackTrace);
+//    Console.WriteLine(ex.Message);
+
+//}
+//catch (SaldoInsuficienteException ex)
+//{
+//    Console.WriteLine("Operação negada ! Saldo Insuficiente !");
+//    Console.WriteLine(ex.Message);
+//}
 
