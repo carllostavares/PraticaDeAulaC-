98abac0d3f682e633ed0a6a81a6b231bc1011706
carlostavares <carlostavares2350@gmail.com>
2023-08-08 17:14:20 -0300

    Adicionando Validações na Class Filme do projeto FilmesAPI.

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class FilmeController : Controller
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody]Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+
+}
